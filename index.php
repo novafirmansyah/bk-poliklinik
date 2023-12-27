0104451264619b7bd367669b02aceb56571fb179
Nova Firmansyah <111202012881@mhs.dinus.ac.id>
2023-12-27 15:45:40 +0700

    polikinik pertama

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+    session_start();
+
+    if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
+        if ($_SESSION['role'] === 'admin') {
+            header('Location: http://'.$_SERVER['HTTP_HOST'].'/bk-poliklinik/views/pages/admin/');
+        } else if ($_SESSION['role'] === 'dokter') {
+            header('Location: http://'.$_SERVER['HTTP_HOST'].'/bk-poliklinik/views/pages/dokter/');
+        }
+    } else {
+        include_once 'views/pages/welcome.php';
+    };
+?>
